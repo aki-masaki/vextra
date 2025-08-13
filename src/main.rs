e452b311f7d8a4ba2aa22d2efdf2a0f112234114
aki-masaki <nikistefan95@gmail.com>
2025-08-13 17:06:36 +0300

    init(cli): setup cli barebones

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+#![allow(dead_code)]
+
+const HELP: &str = "\
+Vextra
+
+USAGE:
+  vextra [OPTIONS] [INPUT]
+
+FLAGS:
+  -h, --help            Prints help information
+
+OPTIONS:
+  --output PATH         Sets an output path [default: /out]
+
+ARGS:
+  <INPUT> The .vex file to parse
+";
+
+#[derive(Debug)]
+struct AppArgs {
+    input: std::path::PathBuf,
+    output: Option<std::path::PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = match parse_args() {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("Error: {}.", e);
+            std::process::exit(1);
+        }
+    };
+
+    println!("{:#?}", args);
+}
+
+fn parse_args() -> Result<AppArgs, pico_args::Error> {
+    let mut pargs = pico_args::Arguments::from_env();
+
+    if pargs.contains(["-h", "--help"]) {
+        print!("{}", HELP);
+        std::process::exit(0);
+    }
+
+    let args = AppArgs {
+        output: pargs.opt_value_from_os_str("--output", parse_path)?,
+        input: pargs.free_from_str()?,
+    };
+
+    pargs.finish();
+
+    Ok(args)
+}
+
+fn parse_path(s: &std::ffi::OsStr) -> Result<std::path::PathBuf, &'static str> {
+    Ok(s.into())
 }
